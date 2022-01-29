98c6ec6c923a191b3dab11e886dddefb463ce0cb
irah rosete <irah.rosete@outlook.com>
2022-01-29 22:11:19 +1100

    style Status component

--- src/components/filter/categories/Status.jsx
+++ src/components/filter/categories/Status.jsx
@@ -5,12 +5,26 @@ const Status = ({ setPageNumber, setStatus }) => {
   let status = ['Alive', 'Dead', 'Unknown']
 
   return (
-    <div>
-      <h2>
-        <button>Status</button>
+    <div className='accordion-item'>
+      <h2 className='accordion-header' id='headingOne'>
+        <button
+          className='accordion-button'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#collapseOne'
+          aria-expanded='true'
+          aria-controls='collapseOne'
+        >
+          Status
+        </button>
       </h2>
-      <div>
-        <div>
+      <div
+        id='collapseOne'
+        className='accordion-collapse collapse show'
+        aria-labelledby='headingOne'
+        data-bs-parent='#accordionExample'
+      >
+        <div className='accordion-body d-flex flex-wrap gap-3'>
           {status.map((item, index) => (
             <FilterButton
               task={setStatus}
