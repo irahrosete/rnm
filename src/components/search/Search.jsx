f823f9556411ee7c77534a49f0a6ba02fe3d655c
irah rosete <irah.rosete@outlook.com>
2022-01-29 18:54:58 +1100

    update Search component styles

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import styles from './Search.module.scss'
 
 const Search = ({ setPageNumber, setSearch }) => {
   let searchButton = (e) => {
     e.preventDefault()
   }
   return (
-    <form>
+    <form
+      className={`${styles.search} d-flex flex-sm-row flex-column align-items-center justify-content-center gap-4 mb-5`}
+    >
       <input
+        className={styles.input}
         onChange={(e) => {
           setPageNumber(1)
           setSearch(e.target.value)
@@ -14,7 +18,12 @@ const Search = ({ setPageNumber, setSearch }) => {
         placeholder='Search for characters'
         type='text'
       ></input>
-      <button onClick={searchButton}>Search</button>
+      <button
+        className={`${styles.btn} btn btn-primary fs-5`}
+        onClick={searchButton}
+      >
+        Search
+      </button>
     </form>
   )
 }
